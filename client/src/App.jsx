95e0a2dd63f23aa1d3e480eea312ca3032aaa2c7
Kodenaime <creativedannyk@gmail.com>
2025-04-13 20:31:08 +0100

    app routing finished

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,41 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
 
-const App = () => {
+import Home from './pages/student/Home'
+import CourseList from './pages/student/CourseList'
+import Coursedetails from './pages/student/Coursedetails'
+import Enrollments from './pages/student/Enrollments'
+import Player from './pages/student/Player'
+import Loading from './components/student/Loading'
+
+import Educator from './pages/educator/Educator'
+import Dashboard from './pages/educator/Dashboard'
+import AddCourse from './pages/educator/AddCourse'
+import Courses from './pages/educator/Courses'
+import Students from './pages/educator/Students'
+
+const App = () => { 
   return (
-    <div>App</div>
+    <div>
+      <Routes>
+
+        <Route path='/' element={<Home />}/>
+        <Route path='/course-list' element={<CourseList />}/>
+        <Route path='/course-list/:input' element={<CourseList />}/>
+        <Route path='/course/:id' element={<Coursedetails />}/>
+        <Route path='/enrollments' element={<Enrollments />}/>
+        <Route path='/player/:courseId' element={<Player />}/>
+        <Route path='/loading/:path' element={<Loading />}/>
+
+        <Route path='/educator' element={<Educator />} >
+          <Route path='/educator' element={<Dashboard />} />
+          <Route path='/add-course' element={<AddCourse />} />
+          <Route path='/courses' element={<Courses />} />
+          <Route path='/students' element={<Students />} />
+        </Route>
+
+      </Routes>
+    </div>
   )
 }
 
